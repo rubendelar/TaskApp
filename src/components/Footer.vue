84529e68c9151f40045249da1148db9c3f391a0e
rubendelar <delolmoruben@gmail.com>
2022-10-10 12:57:21 +0200

    logo de pestaña

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,14 @@
 <template>
   <footer class="bg-light">
-    <!-- Copyright -->
     <div class="text-center p-3 fw-bold">
       © 2022 <img src="../images/logo.png" alt="logo" /> Task App. Build by
       <a
         class="text.primary text-decoration-none"
+        target="_blank"
         href="https://github.com/rubendelar"
         >Rubendelar 🎹</a
       >
     </div>
-    <!-- Copyright -->
   </footer>
 </template>
 
